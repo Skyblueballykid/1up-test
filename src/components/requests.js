a41268b80b9eb2a1376177f00044e55352db2f35
Thomas Kalnik <kalnik.thomas@gmail.com>
2021-01-23 21:55:44 -0600

    create user working

--- src/components/requests.js
+++ src/components/requests.js
@@ -25,9 +25,11 @@ const StyledDiv = styled.div`
   min-height: 60vh;
 `;
 
+const { Search } = Input;
 
-const CLIENT_ID = `f6c4c9623fda20c0a53e9ff18cf45394`;
-const CLIENT_SECRET = `8e83e4b18166fb83dd1771c06a3fbca7`;
+
+const CLIENT_ID = `dbb2e596333400b55c417c0a1ac5187a`;
+const CLIENT_SECRET = `e52c028bd69b7dcfa3587e343d87f13f`;
 const ROOT_API_URL = `https://api.1up.health`;
 const FHIR_API_URL = `https://api.1up.health/fhir`;
 
@@ -39,6 +41,7 @@ function getRequest(value) {
     .catch(err => console.error(err));
 }
 
+
 // adapted from oneup.js file in demo app to use axios 
 function getToken(code, callback) {
     let postUrl = `${ROOT_API_URL}/fhir/ouath2/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${code}&grant_type=authorization_code`;
@@ -46,18 +49,48 @@ function getToken(code, callback) {
 }
 
 
-const response = [
-    {label: 'Authorization', value: 'Authorization'},
-    {label: 'Content Type', value: 'Application/JSON'},
-    {label: 'Connection', value: 'Connection'},
-    {label: 'Transfer-Encoding', value: 'Transfer-Encoding'},
-    {label: 'Keep-Alive', value: 'Keep-Alive'}
-]
+// const response = [
+//     {label: 'Authorization', value: 'Authorization'},
+//     {label: 'Content Type', value: 'Application/JSON'},
+//     {label: 'Connection', value: 'Connection'},
+//     {label: 'Transfer-Encoding', value: 'Transfer-Encoding'},
+//     {label: 'Keep-Alive', value: 'Keep-Alive'}
+// ]
 const onChange = () => console.log('cool');
 
 const Requests = () => {
 
-    const[users, setUsers] = useState([]);
+    const[code, setUserCode] = useState('');
+    const [userData, setUserData] = useState([]);
+
+
+    const createUser = async (value) => {
+         // Custom headers to avoid CORS issue
+         // I am also using the MOESIF Origin & CORS changer Chrome Extension
+        const config = {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          };
+
+        axios.post(`https://api.1up.health/user-management/v1/user/auth-code`, {
+            "app_user_id": value,
+            "client_id": "dbb2e596333400b55c417c0a1ac5187a",
+            "client_secret": "e52c028bd69b7dcfa3587e343d87f13f"
+        }, 
+        config).then((response) => {
+            const data = response.data;
+            const code = response.data.code;
+            setUserCode(code);
+            setUserData(data);
+            console.log(response);
+            console.log(code);
+            console.log(userData);
+
+        });
+    }
+
+    const connectURL = `https://api.1up.health/connect/system/clinical/4707?client_id=dbb2e596333400b55c417c0a1ac5187aaccess_token=f1bdaf944c08d4df29f1c7119e60b69165b12b31` 
 
     return(
         <div>
@@ -65,21 +98,21 @@ const Requests = () => {
         <Title>
         Create User
         </Title>
-        <Text>
-         curl -X POST "https://api.1up.health/user-management/v1/user/auth-code" \
-        -d "app_user_id=myappuserid" \ 
-        -d "client_id=clientidclientidclientid" \
-        -d "client_secret=clientsecretclientsecret"
-    </Text>
-        <JSONViewer json={response}/>
+        <Search placeholder="Enter new user name" style={{ width: 800, margin: '0 10px' }} onSearch={createUser} />
+        <br/>
+        <br/>
+        <br/>
+        <Card>
+        <JSONViewer json={userData}/>
+        </Card>
         </Card>
         <Card>
         <Title>
         Connect
         </Title>
         <Text>
-        <a href="https://api.1up.health/connect/system/clinical/4707?client_id=f6c4c9623fda20c0a53e9ff18cf45394&access_token=dad180fae9bec712cc116ea64627561c793586e3">
-        https://api.1up.health/connect/system/clinical/4707?client_id=f6c4c9623fda20c0a53e9ff18cf45394&access_token=dad180fae9bec712cc116ea64627561c793586e3
+        <a href="https://api.1up.health/connect/system/clinical/4707?client_id=dbb2e596333400b55c417c0a1ac5187a&access_token=f1bdaf944c08d4df29f1c7119e60b69165b12b31">
+        Connect Here
         </a>
         </Text>
         </Card>
@@ -91,7 +124,7 @@ const Requests = () => {
         curl -X GET 'https://api.1up.health/fhir/{`r4`}/Patient/{`1d5e078b47ba`}/$everything' \
         -H "Authorization: Bearer dad180fae9bec712cc116ea64627561c793586e3"
         </Text>
-        <JSONViewer json={response}/>
+        {/* <JSONViewer json={response}/> */}
         </Card>
         </div>
         )
